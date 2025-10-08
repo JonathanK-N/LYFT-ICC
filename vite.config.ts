e9cd5a7cd573a75d1caf2202491dc4045d0c01c6
Jonathan K-N <jkakesa9@gmail.com>
2025-10-08 09:33:33 -0400

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -63,6 +63,7 @@ export default defineConfig({
       },
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg,json}'],
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
         runtimeCaching: [
           {
             urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
