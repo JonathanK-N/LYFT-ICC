09827d8db0979e46d3556163adc24ac59b72749b
Jonathan K-N <jkakesa9@gmail.com>
2025-10-08 07:16:09 -0400

    fix railway deploiement

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,8 @@ export default defineConfig({
   },
   preview: {
     host: '0.0.0.0',
-    port: process.env.PORT ? parseInt(process.env.PORT) : 4173
+    port: process.env.PORT ? parseInt(process.env.PORT) : 4173,
+    allowedHosts: ['lyft-icc-production.up.railway.app', '.railway.app']
   },
   build: {
     outDir: 'dist',
